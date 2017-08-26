6000b1a0d94779161d3fbfb0c0f357b7d62ca964
kingcreek <kingcreek@kingcreek.local>
2017-08-26 04:51:25 +0200

    First Update

---
+++ MainActivity.java
@@ -0,0 +1,129 @@
+package es.kya.test;
+
+import android.app.ActivityManager;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static String TAG = "Servicio";
+    Intent i;
+
+    private ServicioTimer servicioTimer;
+    TextView contador;
+    Button iniciar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        contador = (TextView) findViewById(R.id.mostrar);
+        iniciar = (Button) findViewById(R.id.iniciar);
+        iniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iniciar.setEnabled(false);
+                //inicia el servicio
+                startService(i);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        i = new Intent(this, ServicioTimer.class);
+        bindService(i, mConnection, 0);
+    }
+
+    private BroadcastReceiver br = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            //aqui obtienes los datos enviados por el servicio
+            //obtienes el tiempo que lleva
+            if (intent.getExtras() != null) {
+                if (intent.hasExtra("Tiempo")) {
+                    String tiempo = intent.getStringExtra("Tiempo");
+                    contador.setText(tiempo);
+                } if (intent.hasExtra("Fin")) {
+                    //se recibe que se ha finalizado el contador
+                    String tiempo = intent.getStringExtra("Fin");
+                    contador.setText(tiempo);
+                    iniciar.setEnabled(true);
+                    //cierras el servicio ya que no es necesario mantenerlo, sera creado al pulsar el boton nuevamente
+                    stopService(new Intent(MainActivity.this, ServicioTimer.class));
+                }
+            }
+        }
+    };
+
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName className, IBinder service) {
+
+            ServicioTimer.RunServiceBinder binder = (ServicioTimer.RunServiceBinder) service;
+            servicioTimer = binder.getService();
+            servicioTimer.background();
+            if (servicioTimer.estaEjecutandose()) {
+                iniciar.setEnabled(false);
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        registerReceiver(br, new IntentFilter(ServicioTimer.PAQETE));
+        Log.i(TAG, "Broadcast registrado");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterReceiver(br);
+        Log.i(TAG, "Broadcast desligado");
+    }
+
+    @Override
+    public void onStop() {
+        try {
+            if (servicioTimer.estaEjecutandose()) {
+                servicioTimer.foreground();
+            }
+            unregisterReceiver(br);
+        } catch (Exception e) {}
+        super.onStop();
+    }
+
+
+    @Override
+    public void onDestroy() {
+        Log.i(TAG, "Termina el servicio");
+        super.onDestroy();
+    }
+}
---
+++ ServicioTimer.java
@@ -0,0 +1,115 @@
+package es.kya.test;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+/**
+ * Created by kingcreek on 26/8/17.
+ */
+
+import android.annotation.SuppressLint;
+import android.app.Service;
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.os.IBinder;
+import android.util.Log;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class ServicioTimer extends Service {
+
+    private final IBinder servicioBinder = new RunServiceBinder();
+    public class RunServiceBinder extends Binder {
+        ServicioTimer getService() {
+            return ServicioTimer.this;
+        }
+    }
+
+    boolean estaEjecutandose = false;
+
+    String FORMAT = "%02d:%02d:%02d";
+    private static String TAG = "Servicio";
+    public static final String PAQETE = "com.example.ash.carritosbeta1"; //ejemplo com.proyecto.MainActivity
+    Intent bi = new Intent(PAQETE);
+
+    CountDownTimer cdt = null;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.i(TAG, "Comienza el timer...");
+        estaEjecutandose = true;
+        cdt = new CountDownTimer(9000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                @SuppressLint("DefaultLocale") String tiempo = ""+String.format(FORMAT,
+                        TimeUnit.MILLISECONDS.toHours(millisUntilFinished),
+                        TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished) - TimeUnit.HOURS.toMinutes(
+                                TimeUnit.MILLISECONDS.toHours(millisUntilFinished)),
+                        TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) - TimeUnit.MINUTES.toSeconds(
+                                TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished)));
+
+                //con esto se envia el tiempo
+                bi.putExtra("Tiempo", tiempo);
+                sendBroadcast(bi);
+            }
+            public void onFinish() {
+                //se envia el tiempo finalizado
+                estaEjecutandose = false;
+                bi.putExtra("Fin", "Tiempo terminado!");
+                sendBroadcast(bi);
+                stopSelf();
+            }
+        }.start();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return servicioBinder;
+    }
+
+    public boolean estaEjecutandose() {
+        return estaEjecutandose;
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.e(TAG, "Timer cancelado");
+        super.onDestroy();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        return Service.START_STICKY;
+    }
+
+    public void background() {
+        stopForeground(true);
+    }
+    public void foreground() {
+        startForeground(1, createNotification());
+    }
+
+    private Notification createNotification() {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setContentTitle("Cuenta atras")
+                .setContentText("Pulsa para regresar a la cuenta")
+                .setSmallIcon(R.mipmap.ic_launcher);
+
+        Intent resultIntent = new Intent(this, MainActivity.class);
+        PendingIntent resultPendingIntent =
+                PendingIntent.getActivity(this, 0, resultIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(resultPendingIntent);
+
+        return builder.build();
+    }
+
+}
